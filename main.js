66ea7ef958fbf51b7debe18e3b0b31ce52c0581d
Beth Wilson <bethwilson777@gmail.com>
2022-07-19 20:26:29 -0600

    Addition HTML and CSS with arrays for JS

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-var radioButton = document.getElementById("side")
+//query selectors go here
+
+//array data to use:
+var sides = [
+  Miso Glazed Carrots,
+  Garden Salad,
+  Hush Puppies,
+  Crispy Potatoes,
+  Caeser Salad
+];
+var mainDish = [
+  Thai Yellow Curry,
+  Chicken Fried Rice,
+  BBQ Chicken Burgers,
+  Margarita Pizza,
+  Sheet Pan Fajitas
+];
+var desserts [
+  Black Forest Cake,
+  Banana Bread,
+  Baklava,
+  Macaroons,
+  Chocolate Cupcakes
+];
+
+//event listeners and functions go here:
+//toggle
+//input radio data from arrays
+//create event listeners for let's cook button and clear button. 
