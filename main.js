168f15a6424c613ac5275f44d62e6233e7d7905d
Beth Wilson <bethwilson777@gmail.com>
2022-07-22 18:36:43 -0600

    Adjust the Clear Button outside of the update answer section so I could access the button for an event listener.  Cleaned up some code comments

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//query selectors go here
+
 var radioInputType = document.getElementsByName('side')
 var radioInputMain = document.querySelector('.main-dish')
 var radioInputDessert = document.querySelector('.dessert')
@@ -7,10 +7,9 @@ var letsCookButton = document.querySelector('.lets-cook-button')
 var h2 = document.querySelector('h2')
 var potImage = document.querySelector('.pot-image')
 var answerSection = document.querySelector('.answer-section')
-// var clearButton = document.querySelector('.clear-button')
-//var clearTwoButton = document.querySelector('button').toggle
+var clearButton = document.querySelector('.clear-button')
 
-//array data to use:
+
 var sides = [
   'Miso Glazed Carrots',
   'Garden Salad',
@@ -33,43 +32,41 @@ var desserts = [
   'Chocolate Cupcakes'
 ];
 
-//event listeners and functions go here:
-//create an object variable for the food type and answer
 function getRandomIndex(array) {
   var index = Math.floor(Math.random() * array.length);
   return array[index]
 }
 
-
-var foodType = {
-  answer: 'you should make:',
-  food: 'salmon'
-}
-
 letsCookButton.addEventListener('click', updateAnswer)
 letsCookButton.addEventListener('click', hidePot)
 letsCookButton.addEventListener('click', showAnswer)
-//ADDING NEW BUTTON TAG FOR ONLY THE CLEAR BUTTON TO SHOW AND HIDE
+clearButton.addEventListener('click', clearMessage)
 
 function hidePot(){
   potImage.classList.add('hidden')
+  clearButton.classList.remove('hidden')
 }
 
 function showAnswer(){
   answerSection.classList.remove('hidden')
 }
 
-// function clearMessage(){
-//   answerSection.classList.add('hidden')
-// }
-//add event listeners for the radio buttons to invoke the random food functions created above
+function clearMessage(){
+  answerSection.classList.add('hidden')
+  clearButton.classList.add('hidden')
+  potImage.classList.remove('hidden')
+}
+
+var foodType = {
+  answer: 'you should make:',
+  food: 'salmon'
+}
 
 function updateAnswer() {
   var selectedAnswer =
   document.querySelector('input[name="food-type"]:checked').value;
     if (selectedAnswer === 'side') {
       foodType.food = getRandomIndex(sides)
-      //if answer is the variable name of the radio input selector???
       displayAnswer()
       // h2.innerHTML = `${foodType.answer} Caesar salad`
   } if (selectedAnswer === 'main-dish') {
@@ -91,17 +88,10 @@ function updateAnswer() {
 function displayAnswer(){
   answerSection.innerHTML = '';
   answerSection.innerHTML += `<h1 class="card-two-title"><em>You should make:</em></h1>
-    <p class="card-two-answer">${foodType.food}</p>
-    <button class="clear-button">Clear</button>`
+    <p class="card-two-answer">${foodType.food}</p>`
 }
 
-
-// function displayAnswer(){
-//   h2.innerHTML = `${foodType.answer} ${foodType.food}`
-// }
-
-//displayAnswer() //was invoking the variable foodType on page load.
-
+//<button class="clear-button">Clear</button>
 //toggle
 //input radio data from arrays
 //create event listeners for let's cook button and clear button.
